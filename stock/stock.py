5866e18c96450950498ec69c2857d6321fa71ab4
napthats <thats525@gmail.com>
2015-03-28 21:58:42 +0900

    Rolling mean.

--- stock/stock.py
+++ stock/stock.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import os
+import dateutil.parser
 
-finance = pd.io.excel.read_excel('data/data_20120422.xls')
+finance = pd.io.excel.read_excel('data/finance.xls')
 finance_columns = pd.Series(['code', 'name', 'price', 'pretax_income_rate', 'net_income_rate', 'net_income', 'EPS', 'BPS', 'ROE', 'long_term_debt', 'total_number_stock', 'PER', 'divident', 'PER_new'])
 finance_columns_num = pd.Series([1,1,1,10,10,10,10,10,10,10,10,10,1,1])
 
@@ -24,4 +25,11 @@ file_list = pd.Series(os.listdir(stock_dir))
 stock_dict = {}
 
 for file in file_list:
-    stock_dict[file.split('.')[0]] = pd.read_csv(stock_dir + file,header=None)
+    stock = pd.read_csv(stock_dir + file,header=None,names=['date','open','high','low','close','volume'])
+    #stock.date = stock.date.map(lambda x: str(x)).map(dateutil.parser.parse)
+    stock_dict[file.split('.')[0]] = stock.set_index('date')
+
+stock_df = pd.concat(stock_dict, axis=1)
+stock_df.set_index(pd.Series(stock_df.index).map(lambda x: str(x)).map(dateutil.parser.parse), inplace=True)
+
+rm_stock_df = pd.rolling_mean(stock_df, 50)
