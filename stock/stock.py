55fb38760df1769adc2f288c5d241180a1f7234d
napthats <thats525@gmail.com>
2015-04-05 02:12:24 +0900

    Change extract_incstock

--- stock/stock.py
+++ stock/stock.py
@@ -37,6 +37,7 @@ stock.set_index(pd.Series(stock.index).map(lambda x: str(x)).map(dateutil.parser
 
 
 
+#_, stock_trend = sm.tsa.filters.hpfilter(stock.fillna(method='bfill').fillna(method='ffill'), 1600)
 _, stock_trend = sm.tsa.filters.hpfilter(stock.fillna(method='bfill').fillna(method='ffill'), 1600)
 
 stock_trend_rmax = pd.rolling_max(stock_trend, 400, min_periods=320)
@@ -46,38 +47,49 @@ stock_trend_ismax = stock_trend == stock_trend_rmax
 stock_trend_isdec2inc = pd.rolling_apply(stock_trend, 3, lambda x: x[0] > x[1] and x[1] < x[2]).applymap(lambda x: True if x == 1 else False)
 stock_trend_isinc2dec = pd.rolling_apply(stock_trend, 3, lambda x: x[0] < x[1] and x[1] > x[2]).applymap(lambda x: True if x == 1 else False)
 
-def extract_incstock(ord, window):
-    if ord <= 0 or window <= 0 or ord + window > stock.index.size:
-        return pd.Series()
-    incinc_bool_table = stock_trend_isdec2inc[ord:ord+window].any()
-    incinc_code_array = incinc_bool_table[incinc_bool_table].keys()
-    result_code_list = []
-    for code in incinc_code_array:
-        #a
-        print(code)
-        #a
+def extract_incstock(ord,len):
+    result = pd.DataFrame(columns=['code','max','prev','start'])
+    for code in stock_trend.columns:
+        is_prepare = True
+        search_length = 0
+        start_day = None
+        start_price = None
+        #start_day = stock_trend.index[ord]
+        #start_price = stock_trend[code][ord]
         is_first = True
         previous_max = None
         previous_day = None
+        cup_min = None
         for check_ord in range(ord-1,-1,-1):
-            if stock_trend_isinc2dec[code][check_ord+1]:
+            search_length += 1
+            if is_first and search_length > len:
+                break
+            if is_prepare:
+                if stock_trend_isdec2inc[code][check_ord+1]:
+                    start_day = stock_trend.index[check_ord]
+                    start_price = stock_trend[code][check_ord]
+                    is_prepare = False
+            elif stock_trend_isinc2dec[code][check_ord+1]:
+            #if stock_trend_isinc2dec[code][check_ord+1]:
+                #is_prepare = False
                 if is_first:
-                    print(stock_trend[code][check_ord])
                     if stock_trend_ismax[code][check_ord]:
                         break
                     else:
                         previous_max = stock_trend[code][check_ord]
                         previous_day = stock_trend.index[check_ord]
+                        cup_min = previous_max
                         is_first = False
                 else:
-                    print(stock_trend[code][check_ord])
+                    if stock_trend[code][check_ord] < cup_min:
+                        cup_min = stock_trend[code][check_ord]
                     if stock_trend[code][check_ord] > previous_max:
-                        if stock_trend_ismax[code][check_ord]:
-                            result_code_list.append((code, previous_day, stock_trend_ismax.index[check_ord]))
+                        if stock_trend_ismax[code][check_ord] and stock_trend[code][ord] < previous_max * 1.05 and start_price > (cup_min + stock_trend[code][check_ord]) / 2:
+                            result.loc[result.index.size+1] = [code, stock_trend_ismax.index[check_ord], previous_day, start_day]
                             break
                         else:
                             break
-    return pd.Series(result_code_list)
+    return result
 
 def plot_stock(code):
     stock[code].plot()
